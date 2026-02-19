f1dcb3f67fb7ad973067bd7242bdcf29370ed9c9
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-19 13:16:19 -0600

    feat(ui): setup global styles, typography, and layout structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import "./globals.css";
+
+const plusJakartaSans = Plus_Jakarta_Sans({
+  variable: "--font-plus-jakarta-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Frontend Mentor | Notifications page with Next.js",
+  description: "Mark as read pending notifications!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${plusJakartaSans.variable} font-plus-jakarta-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
