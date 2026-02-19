47cf83a55fc9fa4653b76ebe0b82a4100a4023a9
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-19 13:38:23 -0600

    doc: add notifications data as utils

---
+++ src/utils/data.js
@@ -0,0 +1,71 @@
+export const notificationType = {
+  "reacted": "reacted to your recent post",
+  "commented": "commented on your picture",
+  "followed": "followed you",
+  "joined": "has joined your group",
+  "left": "left the group",
+  "privateMessage": "sent you a private message"
+}
+
+export const data = {
+  "numberOfNotifications": 3,
+  "notifications": [
+    {
+      "userImg": "/images/avatar-mark-webber.webp",
+      "userName": "Mark Webber",
+      "type": "reacted",
+      "content": "My first tournament today!",
+      "time": "1m ago",
+      "seen": false
+    },
+    {
+      "userImg": "/images/avatar-angela-gray.webp",
+      "userName": "Angela Gray",
+      "type": "followed",
+      "content": "",
+      "time": "5m ago",
+      "seen": false
+    },
+    {
+      "userImg": "/images/avatar-jacob-thompson.webp",
+      "userName": "Jacob Thompson",
+      "type": "joined",
+      "content": "Chess Club",
+      "time": "1 day ago",
+      "seen": false
+    },
+    {
+      "userImg": "/images/avatar-rizky-hasanuddin.webp",
+      "userName": "Rizky Hasanuddin",
+      "type": "privateMessage",
+      "content": "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
+      "time": "5 days ago",
+      "seen": true
+    },
+    {
+      "userImg": "/images/avatar-kimberly-smith.webp",
+      "userName": "Kimberly Smith",
+      "type": "commented",
+      "content": "picture",
+      "url": "/images/image-chess.webp",
+      "time": "1 week ago",
+      "seen": true
+    },
+    {
+      "userImg": "/images/avatar-nathan-peterson.webp",
+      "userName": "Nathan Peterson",
+      "type": "reacted",
+      "content": "5 end-game strategies to increase your win rate",
+      "time": "2 weeks ago",
+      "seen": true
+    },
+    {
+      "userImg": "/images/avatar-anna-kim.webp",
+      "userName": "Anna Kim",
+      "type": "left",
+      "content": "Chess Club",
+      "time": "2 weeks ago",
+      "seen": true
+    }
+  ]
+}
