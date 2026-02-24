928861dd755670f44fe034550358fcd5b98a314e
Fernando Pérez Mojica <fernandoperezmojica@gmail.com>
2026-02-23 19:31:25 -0600

    doc: fix notifications data from utils

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,71 +1,69 @@
-export const notificationType = {
-  "reacted": "reacted to your recent post",
-  "commented": "commented on your picture",
-  "followed": "followed you",
-  "joined": "has joined your group",
-  "left": "left the group",
-  "privateMessage": "sent you a private message"
-}
-
 export const data = {
-  "numberOfNotifications": 3,
+  "numberOfNotificationsNotRead": 3,
   "notifications": [
     {
       "userImg": "/images/avatar-mark-webber.webp",
       "userName": "Mark Webber",
       "type": "reacted",
+      "typeDetails": "reacted to your recent post",
       "content": "My first tournament today!",
       "time": "1m ago",
-      "seen": false
+      "isRead": false
     },
     {
       "userImg": "/images/avatar-angela-gray.webp",
       "userName": "Angela Gray",
       "type": "followed",
+      "typeDetails": "followed you",
       "content": "",
       "time": "5m ago",
-      "seen": false
+      "isRead": false
     },
     {
       "userImg": "/images/avatar-jacob-thompson.webp",
       "userName": "Jacob Thompson",
       "type": "joined",
+      "typeDetails": "joined your group",
       "content": "Chess Club",
       "time": "1 day ago",
-      "seen": false
+      "isRead": false
     },
     {
       "userImg": "/images/avatar-rizky-hasanuddin.webp",
       "userName": "Rizky Hasanuddin",
       "type": "privateMessage",
+      "typeDetails": "sent you a private message",
       "content": "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.",
       "time": "5 days ago",
-      "seen": true
+      "isRead": true
     },
     {
       "userImg": "/images/avatar-kimberly-smith.webp",
       "userName": "Kimberly Smith",
       "type": "commented",
+      "typeDetails": "commented on your picture",
       "content": "picture",
       "url": "/images/image-chess.webp",
       "time": "1 week ago",
-      "seen": true
+      "isRead": true
     },
     {
       "userImg": "/images/avatar-nathan-peterson.webp",
       "userName": "Nathan Peterson",
       "type": "reacted",
+      "typeDetails": "reacted to your recent post",
       "content": "5 end-game strategies to increase your win rate",
       "time": "2 weeks ago",
-      "seen": true
+      "isRead": true
     },
     {
       "userImg": "/images/avatar-anna-kim.webp",
       "userName": "Anna Kim",
       "type": "left",
+      "typeDetails": "left the group",
       "content": "Chess Club",
       "time": "2 weeks ago",
-      "seen": true
+      "isRead": true
     }
   ]
 }
